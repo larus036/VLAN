fc038b28db8f2f5600245044b91bfac4ad750663
Roy Raja <Roy.Raja@gmx.de>
2022-06-28 11:40:50 +0200

    test

--- index.php
+++ index.php
@@ -49,22 +49,69 @@ Test
     <img class="toto" src="img/toto.png">
     <p class="title">B 117</p>
     
-    <div class="vlan_cont">
-        <?php 
-        
-        $i= 0;
-        while($i<$portanzahl)
-        {
-            
-            echo ("<div class='vlan'style=background-color:".$vfarbetabelle[$i]['vlan_farbe'].">".$porttabelle[$i]['port_id']);
-            echo ("<span class='text'>Portnr.: ".$i."<br>VLAN ".$porttabelle[$i]['vlan_id']."</span></div>");
-            $i++;
-        }
-
-
-       
-
-        ?>
+    
+         
+        <div class="vlan"style="background-color:blue;color:white">1
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: </span>
+        </div>
+        <div class="vlan">2
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">3
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">4
+            <span class="text">TID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">5
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">11
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">12
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">13
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">14
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">15
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">6
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">7
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">8
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">9
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">10
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">16
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">17
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">18
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">19
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+        <div class="vlan">20
+            <span class="text">ID: 1<br>VLAN: 2<br>Farbe: Blau</span>
+        </div>
+   
         
         
     </div>

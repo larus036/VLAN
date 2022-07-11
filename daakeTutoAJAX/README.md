2deda2f78de4b0389fbff460ca0ea0a6cff3b5f5
Steven Schulze <schulzesteven@gmx.de>
2022-07-11 15:50:38 +0200

    PDF Implementiert aber nicht funktionsfähig. Lässt sich nicht ansprechen

---
+++ daakeTutoAJAX/README.md
@@ -0,0 +1,6 @@
+Für die PDF erstellung brauchen wir Composer.
+
+Einfach runterladen: https://getcomposer.org/download/
+
+Nach der Installation XAMP neustarten (Gegebenenfalls Rechner neu starten) und dann eintippen im Terminal
+"composer require mpdf/mpdf" 
---
+++ daakeTutoAJAX/pdfcreater.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+//Postet Inhalte in Variabeln speichern:
+$vname = $_POST['vname'];
+$nname = $_POST['nname'];
+$email = $_POST['email'];
+$tel = $_POST['phone'];
+$message = $_POST['message'];
+
+//PDF Instanz schaffen
+$mpdf = new \Mpdf\Mpdf();
+
+//PDF machen
+$data = '';
+
+//Überschrift
+$data .= '<h1> Deine Daten=</h1><p>';
+
+$data .= '<strong> Vorname</strong> ' .$vname. '<br />';
+$data .= '<strong> Nachname</strong> ' .$nname. '<br />';
+$data .= '<strong> Email</strong> ' .$email. '<br />';
+$data .= '<strong> Telefonnummer</strong> ' .$tel. '<br />';
+
+if($message){
+    $data .= '<strong> Deine Nachricht</strong> ' .$message. '<br />';
+}
+
+//PDF schreiben
+
+$mpdf->WriteHTML($data);
+
+//Dem Browser schicken
+
+$mpdf->Output('DeinePDF.pdf', 'D');
+
